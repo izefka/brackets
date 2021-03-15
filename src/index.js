986e5a01916c9cc3bd65a5c46e4789734a3e4244
izefka <54077631+izefka@users.noreply.github.com>
2021-03-16 00:09:02 +0300

    fix: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let strArr = str.split('');
+  for (let i = 0; i < strArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strArr[i] == bracketsConfig[j][0]) {
+        if (strArr[i+1] == bracketsConfig[j][1]) {
+          strArr.splice(i, 2);
+          i = -1;
+        }
+      }
+    }
+}
+if (strArr.length == 0) {
+  return true;
+} else {
+  return false;
+}
 }
